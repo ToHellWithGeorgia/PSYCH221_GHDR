779c7d6cf749849811ae697c97c528e233a0c309
Yicheng <Administrator@XT1-20160408FVT.stanford.edu>
2018-12-04 19:39:50 -0800

    Add robust merge and test.

---
+++ src/merge_test.m
@@ -0,0 +1,35 @@
+% Load three Taxi images
+addpath('../scenes');
+load('../scenes/taxi_burst_raw.mat');
+load('../scenes/taxi_merged_raw.mat');
+
+align_depth = 5;
+tile = 16;
+stride = 8;
+search_radius = 8;
+width = size(taxi_raw{1}, 2);
+height = size(taxi_raw{1}, 1);
+gp = cell(1,length(taxi_raw));
+align_offsets = cell(1,length(taxi_raw));
+
+for i = 1:length(taxi_raw)
+    % Raw frame to grayscale, shrink size by 2.
+    grayscale = Bayer2Gray(taxi_raw{i});
+    
+    % Gaussian Pyramid
+    gp{i} = gpPyramid(grayscale, align_depth);
+    % Align.
+    align_offsets{i} = align(gp{1}, gp{i}, width/2, height/2, align_depth, tile, stride, search_radius);
+end
+%%
+% Averaging is not good.
+result1 = avg_merge(taxi_raw, align_offsets, tile, stride);
+figure; subplot 121; imshow(demosaic(uint8(taxi_merged_raw.^0.44*255),'grbg'));
+subplot 122; imshow(demosaic(uint8(result1.^0.44*255),'grbg'));
+
+%%
+result3 = robust_merge(taxi_raw, align_offsets, tile, stride);
+figure; subplot 121; imshow(demosaic(uint8(taxi_merged_raw.^0.44*255),'grbg'));
+subplot 122; imshow(demosaic(uint8(result3.^0.44*255),'grbg')); 
+
+
---
+++ src/robust_merge.m
@@ -0,0 +1,52 @@
+function result = robust_merge(raw_frame, align_offsets, tile, stride)
+    % This function does a simple merge considering the offsets but not
+    % relative accuracy for each frequence.
+    
+    % inputs: a cell array of Bayer raw frame images.
+    % align_offsets: a cell array of 3D tensors representing offset for each
+    % tile
+    assert(length(raw_frame) == length(align_offsets));
+    height = size(raw_frame{1},1);
+    width = size(raw_frame{1},2);
+    result = zeros(height, width);
+    
+    noise_variance = std2(raw_frame{1})^2;
+    
+    cos_1d = zeros(1,16);
+    for x = 1:16
+        cos_1d(x) = 1/2 - 1/2*cos(2*pi*(x+1/2)/16);
+    end
+    cos_2d = cos_1d' * cos_1d;
+
+    for cr = 1:2  % color channel row
+        for cc = 1:2
+            channel = zeros(height/2, width/2);
+            for k = 1:length(raw_frame)
+                for row = 1:stride:height/2-tile
+                    for col = 1:stride:width/2-tile
+                        row_offset = align_offsets{k}((row-1)/stride+1, (col-1)/stride+1, 1);
+                        col_offset = align_offsets{k}((row-1)/stride+1, (col-1)/stride+1, 2);
+                        current_tile = raw_frame{1}(row*2-1+cr-1:2:row*2-1+cr-1+tile*2-1, col*2-1+cc-1:2:col*2-1+cc-1+tile*2-1);
+                        comming_tile = raw_frame{k}((row+row_offset)*2-1+cr-1:2:(row+row_offset)*2-1+cr-1+tile*2-1, ...
+                            (col+col_offset)*2-1+cc-1:2:(col+col_offset)*2-1+cc-1+tile*2-1);
+                        T0 = fft2(current_tile);
+                        Tz = fft2(comming_tile);
+                        Dz = abs(T0 - Tz);
+                        Az = Dz.^2 ./ (Dz.^2 + 8 * noise_variance);
+
+                        channel(row:row+tile-1, col:col+tile-1) = ...
+                            channel(row:row+tile-1, col:col+tile-1) ...
+                            + abs(ifft2(Tz + Az.*(T0 - Tz))).*cos_2d;
+                    end
+                end
+            end
+            result(cr:2:end, cc:2:end) = channel/length(raw_frame);
+        end
+    end
+    result(:,1:tile/2*2) = raw_frame{1}(:,1:tile/2*2);
+    result(:, floor((width/2 - tile) / stride) * stride * 2:end) = ...
+        raw_frame{1}(:, floor((width/2 - tile) / stride) * stride * 2:end);
+    result(1:tile/2*2,:) = raw_frame{1}(1:tile/2*2, :);
+    result(floor((height/2 - tile) / stride) * stride * 2:end,:) = ...
+        raw_frame{1}(floor((height/2 - tile) / stride) * stride * 2:end,:);
+end
